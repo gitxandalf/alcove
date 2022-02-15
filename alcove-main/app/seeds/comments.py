ee506e438bc66ec1aa05d9bfd53a7cbadb0cb215
gitxandalf <alexandergangemi@outlook.com>
2022-02-15 14:27:03 -0800

    added seeder files

---
+++ alcove-main/app/seeds/comments.py
@@ -0,0 +1,35 @@
+from app.models import db, Comment
+
+
+def seed_comments():
+    article_comment_1 = Comment(
+        user_id=1, article_id=1, image_id=None, comment_content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+    image_comment_1 = Comment(
+        user_id=1, article_id=None, image_id=1, comment_content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+    article_comment_2 = Comment(
+        user_id=2, article_id=2, image_id=None, comment_content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+    image_comment_2 = Comment(
+        user_id=2, article_id=None, image_id=2, comment_content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+    article_comment_3 = Comment(
+        user_id=3, article_id=3, image_id=None, comment_content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+    image_comment_3 = Comment(
+        user_id=3, article_id=None, image_id=3, comment_content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+
+    db.session.add(article_comment_1)
+    db.session.add(image_comment_1)
+    db.session.add(article_comment_2)
+    db.session.add(image_comment_2)
+    db.session.add(article_comment_3)
+    db.session.add(image_comment_3)
+
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE the users table.
+# SQLAlchemy doesn't have a built in function to do this
+# TRUNCATE Removes all the data from the table, and RESET IDENTITY
+# resets the auto incrementing primary key, CASCADE deletes any
+# dependent entities
+def undo_comments():
+    db.session.execute('TRUNCATE categories RESTART IDENTITY CASCADE;')
+    db.session.commit()
---
+++ alcove-main/app/seeds/images.py
@@ -0,0 +1,37 @@
+from app.models import db, Image
+
+
+def seed_images():
+    public_image_1 = Image(
+        user_id=1, album_id=1, image_url="https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80", name="Lorem  ipsum", description="""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.""")
+    public_image_2 = Image(
+        user_id=2, album_id=1, image_url="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80", name="Lorem  ipsum", description="""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.""")
+    public_image_3 = Image(
+        user_id=3, album_id=1, image_url="https://images.unsplash.com/photo-1516641396056-0ce60a85d49f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80", name="Lorem  ipsum", description="""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.""")
+    private_image_1 = Image(
+        user_id=1, album_id=2, image_url="https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80", name="Lorem  ipsum", description="""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.""")
+    private_image_2 = Image(
+        user_id=2, album_id=2, image_url="https://images.unsplash.com/photo-1518737003272-dac7c4760d5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1976&q=80", name="Lorem  ipsum", description="""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.""")
+    private_image_3 = Image(
+        user_id=3, album_id=2, image_url="https://images.unsplash.com/photo-1514986888952-8cd320577b68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80", name="Lorem  ipsum", description="""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.""")
+
+
+db.session.add(public_image_1)
+db.session.add(public_image_2)
+db.session.add(public_image_3)
+db.session.add(private_image_1)
+db.session.add(private_image_2)
+db.session.add(private_image_3)
+
+
+db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE the users table.
+# SQLAlchemy doesn't have a built in function to do this
+# TRUNCATE Removes all the data from the table, and RESET IDENTITY
+# resets the auto incrementing primary key, CASCADE deletes any
+# dependent entities
+def undo_images():
+    db.session.execute('TRUNCATE reviews RESTART IDENTITY CASCADE;')
+    db.session.commit()
