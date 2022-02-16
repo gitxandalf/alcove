9ae27a57ac89b7be53b3771df20b6ec78c794766
gitxandalf <alexandergangemi@outlook.com>
2022-02-16 10:49:26 -0800

    made additional user routes to handle requests for albums, images and articles by user id

--- alcove-main/app/api/user_routes.py
+++ alcove-main/app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User
+from app.models import User, Image, Album, Article
 
 user_routes = Blueprint('users', __name__)
 
@@ -18,3 +18,35 @@ def user(id):
     user = User.query.get(id)
     return user.to_dict()
 
+
+@user_routes.route('/<int:id>/images')
+@login_required
+def user_images(id):
+    user = User.query.get(id)
+    images = Image.query.filter(Image.user_id == id).all()
+    return {'user': user.to_dict(), 'images': [image.to_dict() for image in images]}
+
+
+@user_routes.route('/<int:id>/albums')
+@login_required
+def user_albums(id):
+    user = User.query.get(id)
+    albums = Album.query.filter(Album.user_id == id).all()
+    return {'user': user.to_dict(), 'albums': [album.to_dict() for album in albums]}
+
+
+@user_routes.route('/<int:id>/articles')
+@login_required
+def user_articles(id):
+    user = User.query.get(id)
+    articles = Article.query.filter(Article.user_id == id).all()
+    return {'user': user.to_dict(), 'articles': [article.to_dict() for article in articles]}
+
+
+@user_routes.route('/<int:user_id>/albums/<int:album_id>/images')
+@login_required
+def user_albums_images(user_id, album_id):
+    user = User.query.get(user_id)
+    album = Album.query.get(album_id)
+    images = Image.query.filter(Image.album_id == album_id).all()
+    return {'user': user.to_dict(), 'images': [image.to_dict() for image in images], 'album': album.to_dict()}
